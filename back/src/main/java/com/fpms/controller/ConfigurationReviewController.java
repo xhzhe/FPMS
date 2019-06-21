791ec33c3eb94c2a8619951f9b302fe605f4d25e
Lucian <blacklth@qq.com>
2019-06-21 14:18:24 +0800

    完成预先库产品评估逻辑控制-v1.0 -lth 完成配置库产品评估逻辑控制-v1.0 -lth

--- back/src/main/java/com/fpms/controller/ConfigurationReviewController.java
+++ back/src/main/java/com/fpms/controller/ConfigurationReviewController.java
@@ -1,7 +1,10 @@
 package com.fpms.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.fpms.entity.ConfigurationReview;
+import com.fpms.entity.pojo.ResultBean;
+import com.fpms.service.ConfigurationReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author : YongBiao Liao
@@ -12,4 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class ConfigurationReviewController {
+    @Autowired
+    private ConfigurationReviewService configurationReviewService;
+
+    /**
+     *  新增对配置产品的评价
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/21 11:50
+     * @param       configurationReview
+     * @param       productConId
+     * @return     : com.fpms.entity.pojo.ResultBean<java.lang.Boolean>
+     */
+    @PostMapping("/productCon/{productConId}/review")
+    public ResultBean<Boolean> addReview(@RequestBody ConfigurationReview configurationReview, @PathVariable Integer productConId){
+        try{
+            configurationReview.setProductConId(productConId);
+            configurationReviewService.addReview(configurationReview);
+        }
+        catch (Exception e){
+            return new ResultBean<>(e);
+        }
+        return new ResultBean<>(true);
+    }
 }
