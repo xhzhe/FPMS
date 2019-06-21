592c69c1e7f4a638c13733e49e0b6b26e712272b
Lucian <blacklth@qq.com>
2019-06-21 10:51:29 +0800

    完成订单逻辑控制-v1.0 -lth

--- back/src/main/java/com/fpms/dao/ProductCategoryDao.java
+++ back/src/main/java/com/fpms/dao/ProductCategoryDao.java
@@ -13,17 +13,66 @@ import java.util.List;
  */
 @Component
 public interface ProductCategoryDao {
+    /**
+     *  通过主键删除记录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:57
+     * @param       categoryId
+     * @return     : int
+     */
     int deleteByPrimaryKey(Integer categoryId);
 
+    /**
+     *  插入记录的所有字段
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:57
+     * @param       record
+     * @return     : int
+     */
     int insert(ProductCategory record);
 
+    /**
+     *  插入记录的非null字段
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:57
+     * @param       record
+     * @return     : int
+     */
     int insertSelective(ProductCategory record);
 
+    /**
+     *  通过主键查询一条记录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:57
+     * @param       categoryId
+     * @return     : com.fpms.entity.ProductCategory
+     */
     ProductCategory selectByPrimaryKey(Integer categoryId);
 
+    /**
+     *  通过主键更新记录的非null字段
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:57
+     * @param       record
+     * @return     : int
+     */
     int updateByPrimaryKeySelective(ProductCategory record);
 
+    /**
+     *  通过主键更新记录的所有字段
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:58
+     * @param       record
+     * @return     : int
+     */
     int updateByPrimaryKey(ProductCategory record);
 
+    /**
+     *  返回所有记录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 11:58
+     * @param
+     * @return     : java.util.List<com.fpms.entity.ProductCategory>
+     */
     List<ProductCategory> selectAllProductCategory();
 }
--- back/src/main/java/com/fpms/service/ProductCategoryService.java
+++ back/src/main/java/com/fpms/service/ProductCategoryService.java
@@ -11,11 +11,39 @@ import java.util.List;
  * @modified   :
  */
 public interface ProductCategoryService {
+    /**
+     *  新增目录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 16:06
+     * @param       productCategory
+     * @return     : void
+     */
     void addProductCategory(ProductCategory productCategory);
 
+    /**
+     *  更新目录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 16:06
+     * @param       productCategory
+     * @return     : void
+     */
     void updateProductCategory(ProductCategory productCategory);
 
+    /**
+     *  删除目录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 16:07
+     * @param       productCategory
+     * @return     : void
+     */
     void delProductCategory(Integer productCategory);
 
+    /**
+     *  返回所有目录
+     * @author     ：TianHong Liao
+     * @date       ：Created in 2019/6/20 16:07
+     * @param
+     * @return     : java.util.List<com.fpms.entity.ProductCategory>
+     */
     List<ProductCategory> selectAllProductCategory();
 }
