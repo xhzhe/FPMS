cc1598dadaf8d4072d140da217741344d78a20b0
Lucian <blacklth@qq.com>
2019-06-25 14:52:30 +0800

    添加跨域支持配置 -lth

---
+++ back/src/main/java/com/fpms/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.fpms.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : TianHong Liao
+ * @date : 2019/6/25 14:50
+ * @description:跨域支持配置
+ * @modified :
+ */
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080");//允许域名访问，如果*，代表所有域名
+            }
+        };
+    }
+}
