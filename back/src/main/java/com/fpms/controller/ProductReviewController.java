4393bec75344947592a14b7f08c21e02e02d1af5
Lucian <blacklth@qq.com>
2019-06-26 11:32:46 +0800

    修改新增预选库产品评估功能的bug -lth

--- back/src/main/java/com/fpms/controller/ProductReviewController.java
+++ back/src/main/java/com/fpms/controller/ProductReviewController.java
@@ -1,11 +1,15 @@
 package com.fpms.controller;
 
 import com.fpms.annotation.OperationLog;
+import com.fpms.entity.ProductLibraryPre;
 import com.fpms.entity.ProductReview;
 import com.fpms.entity.pojo.ResultBean;
+import com.fpms.service.ProductLibraryPreService;
 import com.fpms.service.ProductReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author : YongBiao Liao
@@ -19,20 +23,34 @@ public class ProductReviewController {
     @Autowired
     private ProductReviewService productReviewService;
 
+    @Autowired
+    private ProductLibraryPreService productLibraryPreService;
     /**
      *  新增对预选库产品的评估
      * @author     ：TianHong Liao
-     * @date       ：Created in 2019/6/21 11:49
-     * @param       productReview
+     * @date       ：Created in 2019/6/26 11:28
+     * @param       param
      * @param       productPreId
      * @return     : com.fpms.entity.pojo.ResultBean<java.lang.Boolean>
      */
     @OperationLog(value = "新增产品评估")
     @PostMapping("/productPre/{productPreId}/review")
-    public ResultBean<Boolean> addReview(@RequestBody ProductReview productReview,@PathVariable Integer productPreId){
+    public ResultBean<Boolean> addReview(@RequestBody Map<String,String> param, @PathVariable Integer productPreId){
         try{
+            //添加评估
+            Integer staffId = Integer.valueOf(param.get("staffId"));
+            Byte productStatus = Byte.valueOf(param.get("productStatus"));
+            ProductReview productReview = new ProductReview();
             productReview.setProductPreId(productPreId);
+            productReview.setStaffId(staffId);
+            productReview.setReviewDesc(param.get("reviewDesc"));
             productReviewService.addReview(productReview);
+            //修改评估状态
+            ProductLibraryPre productLibraryPre = new ProductLibraryPre();
+            productLibraryPre.setProductPreId(productPreId);
+            productLibraryPre.setProductStatus(productStatus);
+            productLibraryPreService.modifyProduct(productLibraryPre);
+
         }
         catch (Exception e){
             return new ResultBean<>(e);
