c64b574671503865e2c66e53c6f3d8405e75ab34
xhz <747367352@qq.com>
2019-07-08 11:22:55 +0800

    新增修改用户birthday格式-xhz

--- back/src/main/java/com/fpms/entity/User.java
+++ back/src/main/java/com/fpms/entity/User.java
@@ -22,7 +22,7 @@ public class User {
     private String userPwd;
 
     private String userGender;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+0")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+0")
     private Date userBrithday;
 
     private String userPhone;
